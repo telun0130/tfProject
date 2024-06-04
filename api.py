d12cd77431e3f9b91a9142eaa801ed72a9847cf3
telun0130 <telunchenworking@gmail.com>
2024-06-04 16:29:13 +0800

    add：templates + homepage.html change：api.py

--- api.py
+++ api.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from predict import predictor
 from train import trainer
 
+app = Flask(__name__)
+
+# @app.route('/')
+
+# if(__name__ == "__main__"):
+#     app.run(debug=True)
